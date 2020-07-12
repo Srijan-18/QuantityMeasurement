02805e0cf14bc51ab0de52ad99c9cb15ee0f1112
Srijan-18 <srijan.mishrasir@gmail.com>
2020-07-12 23:45:50 +0530

    ADDED: Method to get value converted from feet to inches.

--- src/main/java/quantitymeasurement/service/QuantityMeasurement.java
+++ src/main/java/quantitymeasurement/service/QuantityMeasurement.java
@@ -3,6 +3,7 @@ package quantitymeasurement.service;
 public class QuantityMeasurement {
 
     private final int FEET_TO_INCHES_CONVERSION_FACTOR = 12;
+
     public double convert(double feet) {
         return feet * FEET_TO_INCHES_CONVERSION_FACTOR;
     }
