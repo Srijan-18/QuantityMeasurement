632c1cb2517cb7b3737f3b4a8b91979cb9e8d0e3
Srijan-18 <srijan.mishrasir@gmail.com>
2020-07-13 23:59:38 +0530

    ADDED: Model class for feet type

--- src/main/java/quantitymeasurement/model/Feet.java
+++ src/main/java/quantitymeasurement/model/Feet.java
@@ -1,4 +1,23 @@
 package quantitymeasurement.model;
 
+import quantitymeasurement.exception.QuantityMeasurementException;
+
 public class Feet {
+    public double value;
+
+    public Feet(Double value) throws QuantityMeasurementException {
+        try {
+            this.value = value;
+        } catch (NullPointerException e) {
+            throw new QuantityMeasurementException(QuantityMeasurementException.ExceptionType.NULL_VALUE, " NUll Value");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Feet)) return false;
+        Feet feet = (Feet) o;
+        return Double.compare(feet.value, value) == 0;
+    }
 }
