a6020e862dbc14605b657957b71390c09b67ecfc
Srijan-18 <srijan.mishrasir@gmail.com>
2020-07-14 07:12:26 +0530

    REFACTORED: Removed reduandant code.

--- src/main/java/quantitymeasurement/service/QuantityMeasurement.java
+++ src/main/java/quantitymeasurement/service/QuantityMeasurement.java
@@ -4,19 +4,12 @@ import quantitymeasurement.utility.ConversionType;
 
 public class QuantityMeasurement {
     /**
-     * TASK: to convert from feet to inches.
-     * @param feet
+     * TASK : To convert one type to another.
+     * @param value
+     * @param conversionType
      * @return
      */
-    public double convert(double feet) {
-        return new ConversionFactor().feetToInch(feet);
-    }
-
-    public<T> boolean compare ( T value1, T value2) {
-        return value1.equals(value2);
-    }
-
-    public boolean checkConversion(double value1, double value2, ConversionType conversionType) {
-        return value1*conversionType.value == value2;
+    public double doConversion(double value, ConversionType conversionType) {
+        return value*conversionType.value;
     }
 }
