12a664c12b2156fc5830a32d1a905a4ce70e1bf7
Srijan-18 <srijan.mishrasir@gmail.com>
2020-07-14 06:47:14 +0530

    ADDED: Enum Constant to convert between inch and yard.

--- src/main/java/quantitymeasurement/utility/ConversionType.java
+++ src/main/java/quantitymeasurement/utility/ConversionType.java
@@ -1,7 +1,7 @@
 package quantitymeasurement.utility;
 
 public enum ConversionType {
-    FEET_TO_YARD(3);
+    FEET_TO_YARD(3), INCH_TO_YARD(36);
 
     public double value;
     ConversionType(double value) {
