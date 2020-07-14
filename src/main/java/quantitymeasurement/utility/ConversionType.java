bbc687e0650797bb54a0d32ec4cb72ab4e0c1f77
Srijan-18 <srijan.mishrasir@gmail.com>
2020-07-14 07:19:50 +0530

    ADDED: Enum constant to convert from yards to feet.

--- src/main/java/quantitymeasurement/utility/ConversionType.java
+++ src/main/java/quantitymeasurement/utility/ConversionType.java
@@ -1,7 +1,7 @@
 package quantitymeasurement.utility;
 
 public enum ConversionType {
-    FEET_TO_YARD(1.0/3.0), INCH_TO_YARD(1.0/36.0), YARD_TO_INCH(36.0);
+    FEET_TO_YARD(1.0/3.0), INCH_TO_YARD(1.0/36.0), YARD_TO_INCH(36.0), YARD_TO_FEET(3.0);
 
     public double value;
     ConversionType(double value) {
