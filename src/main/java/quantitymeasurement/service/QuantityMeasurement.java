e6d8df750f28bf66a466309cab6a1f0e060753df
Srijan-18 <srijan.mishrasir@gmail.com>
2020-07-16 05:18:26 +0530

    ADDED: Method to perform addition of two length values.

--- src/main/java/quantitymeasurement/service/QuantityMeasurement.java
+++ src/main/java/quantitymeasurement/service/QuantityMeasurement.java
@@ -12,4 +12,9 @@ public class QuantityMeasurement {
     public double convertToCommonBase(double value, Length.LengthType lengthType) {
         return value* lengthType.conversionToBaseFactor;
     }
+
+    public double addValues(Length length1, Length length2) {
+        return convertToCommonBase(length1.value, length1.lengthType)
+               + convertToCommonBase(length2.value, length2.lengthType) ;
+    }
 }
