e26267b041d98dfa31d2310adf8bf5f00933575e
Srijan-18 <srijan.mishrasir@gmail.com>
2020-07-16 12:14:34 +0530

    REFACTORED: Removed a dedidcated method to check negative value.

--- src/main/java/quantitymeasurement/service/QuantityMeasurement.java
+++ src/main/java/quantitymeasurement/service/QuantityMeasurement.java
@@ -7,7 +7,7 @@ import quantitymeasurement.model.Unit;
 public class QuantityMeasurement {
 
     /**
-     * TASK : To convert one type to A common base.
+     * TASK : To convert one type to a common base.
      * @param value
      * @param unitType
      * @return
@@ -25,13 +25,14 @@ public class QuantityMeasurement {
      * @return result in common base unit.
      */
     public double addValues(Unit unit1, Unit unit2) {
-        this.checkNegative(unit1);
-        this.checkNegative(unit2);
+        if(unit1.value < 0.0 || unit2.value < 0.0 )
+            throw new QuantityMeasurementException(QuantityMeasurementException.ExceptionType.NEGATIVE_VALUE,
+                                                   "Negative Value");
         if(unit1.unitType.unitCategory.equals(unit2.unitType.unitCategory))
             return convertToCommonBase(unit1.value, unit1.unitType)
                     + convertToCommonBase(unit2.value, unit2.unitType) ;
         throw new QuantityMeasurementException(QuantityMeasurementException.ExceptionType.DIFFERENT_CATEGORIES,
-                                                "DIFFERENT UNIT CATEGORIES");
+                                               "DIFFERENT UNIT CATEGORIES");
     }
 
     /**
@@ -41,24 +42,14 @@ public class QuantityMeasurement {
      * @return result of comparison in boolean format
      */
     public boolean compare(Unit unit1, Unit unit2) {
-        this.checkNegative(unit1);
-        this.checkNegative(unit2);
+        if(unit1.value < 0.0 || unit2.value < 0.0 )
+            throw new QuantityMeasurementException(QuantityMeasurementException.ExceptionType.NEGATIVE_VALUE,
+                                                   "Negative Value");
         if (unit1.unitType.unitCategory.equals(unit2.unitType.unitCategory))
             return compareConvertedValues(convertToCommonBase(unit1.value, unit1.unitType),
                                           convertToCommonBase(unit2.value, unit2.unitType));
         throw new QuantityMeasurementException(QuantityMeasurementException.ExceptionType.DIFFERENT_CATEGORIES,
-                                                "DIFFERENT UNIT CATEGORIES");
-    }
-
-    /**
-     * TASK: To Check for Negative value.
-     * @param unit
-     */
-
-    private void checkNegative(Unit unit) {
-        if(unit.value< 0.0 )
-            throw new QuantityMeasurementException(QuantityMeasurementException.ExceptionType.NEGATIVE_VALUE,
-                                                    "Negative Value");
+                                               "DIFFERENT UNIT CATEGORIES");
     }
 
     /**
