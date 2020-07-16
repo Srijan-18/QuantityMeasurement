726f888b93a414e131dc11a575a7f56ccbd8e3e4
Srijan-18 <srijan.mishrasir@gmail.com>
2020-07-16 07:49:23 +0530

    ADDED: Test case to validate exception when comparing two values of different unit categories.

--- src/test/java/QuantityMeasurementTest.java
+++ src/test/java/QuantityMeasurementTest.java
@@ -7,10 +7,10 @@ import quantitymeasurement.service.QuantityMeasurement;
 
 public class QuantityMeasurementTest {
 
-    private  QuantityMeasurement quantityMeasurement;
+    private QuantityMeasurement quantityMeasurement;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         quantityMeasurement = new QuantityMeasurement();
     }
 
@@ -30,7 +30,7 @@ public class QuantityMeasurementTest {
     @Test
     public void givenFeetValue_WhenNull_ShouldThrowAnException() {
         try {
-            new Unit(null, Unit.UnitType.FEET );
+            new Unit(null, Unit.UnitType.FEET);
         } catch (QuantityMeasurementException e) {
             Assert.assertEquals(QuantityMeasurementException.ExceptionType.NULL_VALUE, e.type);
         }
@@ -45,10 +45,11 @@ public class QuantityMeasurementTest {
 
     @Test
     public void givenDifferentValuesInSameFrom_WhenCompared_ShouldReturnFalse() throws QuantityMeasurementException {
-        Unit feetValue1 = new Unit(24.0, Unit.UnitType.FEET );
+        Unit feetValue1 = new Unit(24.0, Unit.UnitType.FEET);
         Unit feetValue2 = new Unit(36.0, Unit.UnitType.FEET);
         Assert.assertFalse(feetValue1.equals(feetValue2));
     }
+
     @Test
     public void given0InchAnd0Inch_ShouldReturnTrue() throws QuantityMeasurementException {
         Unit inchValue1 = new Unit(0.0, Unit.UnitType.INCH);
@@ -58,7 +59,7 @@ public class QuantityMeasurementTest {
 
     @Test
     public void givenInchAndOtherForm_WhenCompared_ShouldThrowCustomException() throws QuantityMeasurementException {
-        Unit inchValue = new Unit(2.0 , Unit.UnitType.INCH);
+        Unit inchValue = new Unit(2.0, Unit.UnitType.INCH);
         Unit lengthValue = new Unit(2.0, Unit.UnitType.FEET);
         Assert.assertFalse(inchValue.equals(lengthValue));
     }
@@ -92,7 +93,7 @@ public class QuantityMeasurementTest {
     public void given1YardAnd3Feet_WhenCompared_ShouldReturnTrue() throws QuantityMeasurementException {
         Unit feet = new Unit(3.0, Unit.UnitType.FEET);
         Unit yard = new Unit(1.0, Unit.UnitType.YARD);
-       Assert.assertTrue(quantityMeasurement.compare(feet, yard));
+        Assert.assertTrue(quantityMeasurement.compare(feet, yard));
     }
 
     @Test
@@ -106,21 +107,21 @@ public class QuantityMeasurementTest {
     public void given1InchAnd1Yard_WhenCompared_ShouldReturnFalse() throws QuantityMeasurementException {
         Unit inch = new Unit(1.0, Unit.UnitType.INCH);
         Unit yard = new Unit(1.0, Unit.UnitType.YARD);
-       Assert.assertFalse(quantityMeasurement.compare(inch, yard));
+        Assert.assertFalse(quantityMeasurement.compare(inch, yard));
     }
 
     @Test
     public void given1YardAnd36Inch_WhenCompared_ShouldReturnTrue() throws QuantityMeasurementException {
         Unit inch = new Unit(36.0, Unit.UnitType.INCH);
         Unit yard = new Unit(1.0, Unit.UnitType.YARD);
-       Assert.assertTrue(quantityMeasurement.compare(inch, yard));
+        Assert.assertTrue(quantityMeasurement.compare(inch, yard));
     }
 
     @Test
     public void given36InchANd1Yard_WhenCompared_ShouldReturnTrue() throws QuantityMeasurementException {
         Unit inch = new Unit(36.0, Unit.UnitType.INCH);
         Unit yard = new Unit(1.0, Unit.UnitType.YARD);
-       Assert.assertTrue(quantityMeasurement.compare(yard, inch));
+        Assert.assertTrue(quantityMeasurement.compare(yard, inch));
     }
 
     @Test
@@ -155,8 +156,8 @@ public class QuantityMeasurementTest {
 
     @Test
     public void given2InchAnd2Inch_WhenAdded_ShouldReturn4Inch() {
-        Unit inch1 = new Unit(2.0 , Unit.UnitType.INCH);
-        Unit inch2 = new Unit(2.0 , Unit.UnitType.INCH);
+        Unit inch1 = new Unit(2.0, Unit.UnitType.INCH);
+        Unit inch2 = new Unit(2.0, Unit.UnitType.INCH);
         double result = quantityMeasurement.addValues(inch1, inch2);
         Assert.assertEquals(result, 4.0, 0.0001);
     }
@@ -164,7 +165,7 @@ public class QuantityMeasurementTest {
     @Test
     public void given1FeetAnd2Inches_WhenAdded_ShouldReturn14Inches() {
         Unit feet = new Unit(1.0, Unit.UnitType.FEET);
-        Unit inch = new Unit(2.0 , Unit.UnitType.INCH);
+        Unit inch = new Unit(2.0, Unit.UnitType.INCH);
         double result = quantityMeasurement.addValues(feet, inch);
         Assert.assertEquals(result, 14.0, 0.0001);
     }
@@ -179,7 +180,7 @@ public class QuantityMeasurementTest {
 
     @Test
     public void givenValuesInInchesAndCentimeter_WhenAdded_ShouldReturnResultInInches() {
-        Unit inch = new Unit(2.0 , Unit.UnitType.INCH);
+        Unit inch = new Unit(2.0, Unit.UnitType.INCH);
         Unit centimeter = new Unit(2.5, Unit.UnitType.CM);
         double result = quantityMeasurement.addValues(inch, centimeter);
         Assert.assertEquals(result, 3.0, 0.0001);
@@ -270,6 +271,17 @@ public class QuantityMeasurementTest {
         Unit kilogram = new Unit(1250.0, Unit.UnitType.KILOGRAM);
         try {
             quantityMeasurement.addValues(litre, kilogram);
+        } catch (QuantityMeasurementException e) {
+            Assert.assertEquals(QuantityMeasurementException.ExceptionType.DIFFERENT_CATEGORIES, e.type);
+        }
+    }
+
+    @Test
+    public void givenValuesToCompare_WhenUnitCategoriesMismatch_ShouldThrowAnException() {
+        Unit litre = new Unit(1.0, Unit.UnitType.LITRE);
+        Unit kilogram = new Unit(1250.0, Unit.UnitType.KILOGRAM);
+        try {
+            quantityMeasurement.compare(litre, kilogram);
         } catch (QuantityMeasurementException e) {
             Assert.assertEquals(QuantityMeasurementException.ExceptionType.DIFFERENT_CATEGORIES, e.type);
         }
